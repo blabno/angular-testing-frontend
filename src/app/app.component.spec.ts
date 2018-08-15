bc6ea928bdb14844f929c11da5a4295aaf902514
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-08-15 08:50:20 +0200

    Unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ApiService } from './api.service';
+import { NEVER, of } from 'rxjs';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let apiServiceStub;
+  let apiReply;
+  beforeEach(() => {
+    apiServiceStub = jasmine.createSpyObj('ApiService', ['getQuoteForToday']);
+    apiReply = `${Math.random()}`;
+    apiServiceStub.getQuoteForToday.and.returnValue(of(apiReply));
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      providers: [{ provide: ApiService, useValue: apiServiceStub }]
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+  });
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
+  });
+  it('should fetch quote for today from the backend', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    expect(app.quote).toBeUndefined();
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-testing-frontend!');
-  }));
+    expect(apiServiceStub.getQuoteForToday).toHaveBeenCalledTimes(1);
+    expect(app.quote).toEqual(apiReply);
+  });
+  describe('before reply arrives from the apiService', () => {
+    it('should render "Loading..." text', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const compiled = fixture.debugElement.nativeElement;
+      apiServiceStub.getQuoteForToday.and.returnValue(NEVER);
+      fixture.detectChanges();
+      expect(compiled.textContent.trim()).toEqual('Loading...');
+    });
+  });
+  describe('when reply arrives from the apiService', () => {
+    it('should render quote in a h1 tag', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.textContent.trim()).toEqual(`Quote for today: "${apiReply}"`);
+      expect(compiled.querySelector('h1').textContent.trim()).toEqual(`Quote for today: "${apiReply}"`);
+    });
+  });
 });
